53a8ad22581e173c04bb3756992d4b1463150c69
thienduyen12930 <thienduyen1209@gmail.com>
2023-09-29 14:16:14 +0700

    solvingEquation

---
+++ src/newpackage/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+import controller.SolvingEquation;
+public class Main {
+     public static void main(String[] args) {
+            SolvingEquation solvingEquation = new SolvingEquation();
+            solvingEquation.runMenu();
+
+    }
+}
